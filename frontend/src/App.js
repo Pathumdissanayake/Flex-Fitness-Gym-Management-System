414d9d9569d4266c8e430e79208e5619220bf805
IT21258480 <100502665+Pathumdissanayake@users.noreply.github.com>
2023-06-23 08:59:51 +0530

    Updated Flex Fitness App Version 5.0.0 (Integrated Deployment)

---
+++ frontend/src/App.js
@@ -0,0 +1,40 @@
+import "bulma/css/bulma.min.css";
+import {BrowserRouter}from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css'
+import Navigation from "./components/Online-shopping-components/Navigation";
+import ThemeProvider from './components/Online-shopping-components/theme';
+import Onlineshop_routes from "./routes/Onlineshop_routes";
+import Footer from "./components/Online-shopping-components/Footer";
+import ScrollToTop from './components/Online-shopping-components/components/scroll-to-top';
+import Router from './components/Online-shopping-components/routes'
+
+function App() {
+  //header
+ 
+return(
+    <BrowserRouter>
+  
+      
+    
+   <Navigation/>
+   <Router/>
+
+
+
+    <footer id = "tttt">
+          <div className="text-center">All rights reserved</div>
+        </footer>
+  
+  
+   
+    
+
+
+   </BrowserRouter>
+  
+  
+)
+
+}
+
+export default App;
